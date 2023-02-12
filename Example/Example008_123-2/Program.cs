15ed26cc7e218a32d3d0a37008e41d8d204399e5
Khromov_Mikhail <khrom1@yandex.ru>
2023-02-13 00:53:45 +0300

    add Example 008

---
+++ Example/Example008_123-2/Program.cs
@@ -0,0 +1,28 @@
+﻿
+// Найти вторую цифру трёх значного числа.
+
+//value = 123
+
+//value div  100 = 1
+//       /
+//неполное частное
+
+//value mod  100 = 23
+//        %
+//остаток от деления
+
+//value div  10 = 12
+//неполное частное
+
+//value mod  10 = 1
+//остаток от деления
+
+//value div  10
+
+//(value div  100) div  10
+
+//(value div  10) value mod  10
+
+int value = 123;
+Console.WriteLine((value % 100) / 10);
+System.Console.WriteLine((value / 10) % 10);
