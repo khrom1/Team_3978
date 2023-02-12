aedb65b6497da7805e0090969717011d3820cd03
Khromov_Mikhail <khrom1@yandex.ru>
2023-02-12 20:58:23 +0300

    add Еxample 005 Condition If-Else

---
+++ Example/Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "миша")
+{
+    Console.WriteLine("Ура это же Миша");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.Write(username);
+}
--- Example/Еxample004_Div/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿double numberA = 16;
-double numberB = 5;
-double result = (numberA / numberB);
-Console.WriteLine(result);
-
