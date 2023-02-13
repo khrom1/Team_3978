65514df412463fcb06a7851626ba2139ae70f8e3
Khromov_Mikhail <khrom1@yandex.ru>
2023-02-13 04:20:52 +0300

    Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).

---
+++ DZ_C#/DZ_C#004/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        int n;
+        
+        Console.WriteLine("Введите N ");
+        n = (int) inputValue();
+        int i;
+        
+        i = 2;
+        while (i <= n)
+        {
+            Console.Write(i.ToString() + ",");
+            i = i + 2;
+        }
+    }
+    
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result));
+        return result;
+    }
+}
+
