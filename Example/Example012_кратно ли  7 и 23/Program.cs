0a74427be659bcd3213a0b96c661507d3eb4fb5f
Khromov_Mikhail <khrom1@yandex.ru>
2023-02-13 15:30:25 +0300

    Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно

---
+++ Example/Example012_кратно ли  7 и 23/Program.cs
@@ -0,0 +1,17 @@
+﻿// . Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно
+//7 и 23.
+
+//14 -> нет
+//46 -> нет
+//161 -> да
+//double a = Convert.ToDouble(Console.ReadLine());
+//if ((a % 7 == 0) && (a % 23 == 0)) Console.WriteLine("Yes");
+//else
+//Console.WriteLine("No");
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 7 == 0 && n % 23 == 0)
+Console.WriteLine("yes");
+else
+Console.WriteLine("no");
